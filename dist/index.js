9c369707adabe9f9adc0af042376ac1302d36796
NessMC <contact@nessmc.fr>
2020-06-28 21:14:43 +0200

    🔨 FIles execution added.

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,32 @@
 #!/usr/bin/env node
+"use strict";
+/*//////////////////////////////////
+         POPSCRIPT LANGUAGE
+                Main
+//////////////////////////////////*/
+exports.__esModule = true;
+var core_1 = require("@popscript/core");
+var PATH = require("path");
+var FS = require("fs");
+var CLI = /** @class */ (function () {
+    function CLI() {
+        this.arguments = process.argv.slice(2);
+        this.folder = process.cwd();
+    }
+    CLI.prototype.init = function () {
+        var _this = this;
+        if (this.arguments.filter(function (x) { return ['--input', '-input', '-i', '--i'].includes(x); }).length > 0) {
+            var index = this.arguments.findIndex(function (x) { return ['--input', '-input', '-i', '--i'].includes(x); }), input_1 = this.arguments.slice(index + 1, index + 2).length > 0 ? this.arguments.slice(index + 1, index + 2)[0] : undefined;
+            if (!input_1)
+                throw new Error('No files were specified!');
+            FS.exists(PATH.join(this.folder, input_1), function (bool) {
+                if (!bool)
+                    throw new Error('File specified does not exist!');
+                new core_1["default"]().file(PATH.join(_this.folder, input_1));
+            });
+        }
+    };
+    return CLI;
+}());
+exports["default"] = CLI;
+new CLI().init();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
 #!/usr/bin/env node
+/*//////////////////////////////////
+         POPSCRIPT LANGUAGE
+                Main
+//////////////////////////////////*/
+
+import Popscript from '@popscript/core'
+import * as PATH from 'path'
+import * as FS   from 'fs'
+
+export default class CLI {
+
+    private readonly arguments : Array<string> = process.argv.slice(2)
+    private readonly folder    : string        = process.cwd()
+
+    constructor () {}
+
+    public init () {
+
+        if (this.arguments.filter(x => ['--input', '-input', '-i', '--i'].includes(x)).length > 0) {
+            const index = this.arguments.findIndex(x => ['--input', '-input', '-i', '--i'].includes(x)),
+                  input = this.arguments.slice(index + 1, index + 2).length > 0 ? this.arguments.slice(index + 1, index + 2)[0] : undefined
+            if (!input) throw new Error('No files were specified!')
+            
+            FS.exists(PATH.join(this.folder, input), bool => {
+                if (!bool) throw new Error('File specified does not exist!')
+                new Popscript().file(PATH.join(this.folder, input))
+            })
+            
+        }
+
+    }
+
+}
+
+new CLI().init()
